3a62e821361104b176b919de1a6eef7441bc5bce
sali1991 <sali.haguli@spotinst.com>
2019-11-06 15:19:39 +0200

    including the tests

--- spotinst/commons/managed_instance_aws/consts.go
+++ spotinst/commons/managed_instance_aws/consts.go
@@ -2,9 +2,7 @@ package managed_instance_aws
 
 import "github.com/terraform-providers/terraform-provider-spotinst/spotinst/commons"
 
-//todo sali change file name from 'const' to 'consts'
 const (
-	//ManagedInstance commons.FieldName = "managed_instance"
 	Name        commons.FieldName = "name"
 	Description commons.FieldName = "description"
 	Region      commons.FieldName = "region"
