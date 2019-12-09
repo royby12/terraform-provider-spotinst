1cc65f72d4d862e17f2f81e4e17ef901d5ef2c31
sali1991 <sali.haguli@spotinst.com>
2019-12-09 18:55:56 +0200

    [src] resource/spotinst_elastigroup_aws_beanstalk: added `scheduled_task`-4

--- spotinst/resource_spotinst_elastigroup_aws_beanstalk.go
+++ spotinst/resource_spotinst_elastigroup_aws_beanstalk.go
@@ -1,22 +1,20 @@
 package spotinst
 
 import (
+	"context"
 	"fmt"
-	"github.com/terraform-providers/terraform-provider-spotinst/spotinst/elastigroup_aws_beanstalk_scheduled_task"
 	"log"
+	"strings"
+	"time"
 
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/spotinst/spotinst-sdk-go/service/elastigroup/providers/aws"
 	"github.com/spotinst/spotinst-sdk-go/spotinst"
 	"github.com/spotinst/spotinst-sdk-go/spotinst/client"
 	"github.com/terraform-providers/terraform-provider-spotinst/spotinst/commons"
-
-	"context"
-	"strings"
-	"time"
-
-	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/terraform-providers/terraform-provider-spotinst/spotinst/elastigroup_aws_beanstalk"
+	"github.com/terraform-providers/terraform-provider-spotinst/spotinst/elastigroup_aws_beanstalk_scheduled_task"
 )
 
 func resourceSpotinstElastigroupAWSBeanstalk() *schema.Resource {
